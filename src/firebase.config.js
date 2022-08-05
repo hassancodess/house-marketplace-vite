02c3db06b04ab0106dc21bcbb8085e049f0942c1
hassancodess <dev.hassancodes@gmail.com>
2022-08-05 12:16:04 +0500

    Add .env

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -6,12 +6,12 @@ import { getFirestore } from 'firebase/firestore'
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyA4axJX1IOQD2LU06VsES1AHonho0VdZSE',
-  authDomain: 'house-marketplace-app-4ab78.firebaseapp.com',
-  projectId: 'house-marketplace-app-4ab78',
-  storageBucket: 'house-marketplace-app-4ab78.appspot.com',
-  messagingSenderId: '878223790735',
-  appId: '1:878223790735:web:dd0438a10704215b7f16fa',
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
 }
 
 // Initialize Firebase
