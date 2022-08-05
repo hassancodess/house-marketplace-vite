adc849aeb618133d9533b5f662a219c8cb3401d6
hassancodess <dev.hassancodes@gmail.com>
2022-08-05 08:45:50 +0500

    Add SignIn & SignUp Pages

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,7 +1,88 @@
-import React from 'react'
-
+import { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { ReactComponent as ArrowRightIcon } from '../assets/svg/keyboardArrowRightIcon.svg'
+import visibilityIcon from '../assets/svg/visibilityIcon.svg'
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth'
+import OAuth from '../components/OAuth'
+import { toast } from 'react-toastify'
 function SignIn() {
-  return <div>SignIn</div>
+  const [showPassword, setShowPassword] = useState(false)
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  })
+  const { email, password } = formData
+  const navigate = useNavigate()
+
+  const onChange = (e) => {
+    setFormData((prev) => ({
+      ...prev,
+      [e.target.id]: e.target.value,
+    }))
+  }
+  const onSubmit = async (e) => {
+    e.preventDefault()
+    try {
+      const auth = getAuth()
+      const userCredential = await signInWithEmailAndPassword(
+        auth,
+        email,
+        password
+      )
+      if (userCredential.user) {
+        navigate('/profile')
+      }
+    } catch (error) {
+      toast.error('Something went wrong with Sign In')
+    }
+  }
+  return (
+    <>
+      <div className='pageContainer'>
+        <header>
+          <p className='pageHeader'>Welcome Back!</p>
+        </header>
+        <form onSubmit={onSubmit}>
+          <input
+            type='text'
+            placeholder='Email'
+            className='emailInput'
+            id='email'
+            value={email}
+            onChange={onChange}
+          />
+          <div className='passwordInputDiv'>
+            <input
+              type={showPassword ? 'text' : 'password'}
+              placeholder='Password'
+              className='passwordInput'
+              id='password'
+              value={password}
+              onChange={onChange}
+            />
+            <img
+              src={visibilityIcon}
+              className='showPassword'
+              onClick={() => setShowPassword((prev) => !prev)}
+            />
+          </div>
+          <Link to='/forgot-password' className='forgotPasswordLink'>
+            Forgot Password?
+          </Link>
+          <div className='signInBar'>
+            <p className='signInText'>Sign In</p>
+            <button className='signInButton'>
+              <ArrowRightIcon width='34px' height='34px' fill='#ffffff' />
+            </button>
+          </div>
+        </form>
+        <OAuth />
+        <Link to='/sign-up' className='registerLink'>
+          Sign Up Instead
+        </Link>
+      </div>
+    </>
+  )
 }
 
 export default SignIn
