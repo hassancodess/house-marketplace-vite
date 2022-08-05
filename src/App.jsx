a1e777da8a400a2ec7743227ae7259a962547fe1
hassancodess <dev.hassancodes@gmail.com>
2022-08-05 08:31:27 +0500

    Add Routes, Navbar & ToastContainer

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,39 @@
+// Router
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+// Pages
+import ForgotPassword from './pages/ForgotPassword'
+import Explore from './pages/Explore'
+import Profile from './pages/Profile'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import Offers from './pages/Offers'
+// Private Route
+import PrivateRoute from './components/PrivateRoute'
+// React Toastify
+import { ToastContainer, toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+// Components
+import Navbar from './components/Navbar'
+// App.jsx
 function App() {
-  return <div className='App'></div>
+  return (
+    <div className='App'>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Explore />} />
+          <Route element={<PrivateRoute />}>
+            <Route path='/profile' element={<Profile />} />
+          </Route>
+          <Route path='/sign-in' element={<SignIn />} />
+          <Route path='/sign-up' element={<SignUp />} />
+          <Route path='/offers' element={<Offers />} />
+          <Route path='/forgot-password' element={<ForgotPassword />} />
+        </Routes>
+        <Navbar />
+      </Router>
+      <ToastContainer />
+    </div>
+  )
 }
 
 export default App
